ef4fdcba737af1db7525dde05aa1316b0214673e
veronikavavrochova <veronika.vavrochova@infis.cz>
2025-04-02 13:17:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+# Seznam pro ukládání výsledků
+cisla = [1,5,10,20,25,30]
+while True:
+    #Požádáme uživatele o dvě čísla a znaménko
+    cislo1 = int(input("Zadej první čislo:"))
+    cislo2 = int(input("Zadej druhé číslo"))
+    znamenko = (input("Zadejte znamenko"))
+    if znamenko ==  "+":
+        vysledek = cislo1+cislo2
+        print("vysledek")
+    elif znamenko == "-"
+    vysledek == cislo1-cislo2
+    print("vysledek")
+    elif znamenko == "*":
+    vysledek == cislo1*cislo2
+       
+    print("vysledek")
